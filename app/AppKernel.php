bb58e14bad91e2d0bbde023e27f931a813b9edc2
Mark Rushton <mark@modernfidelity.co.uk>
2016-02-09 20:04:25 +0000

    jms/serializer added

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,33 +3,35 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel
-{
-    public function registerBundles()
-    {
-        $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
-        );
+class AppKernel extends Kernel {
+  public function registerBundles() {
+    $bundles = array(
+      new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+      new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+      new Symfony\Bundle\TwigBundle\TwigBundle(),
+      new Symfony\Bundle\MonologBundle\MonologBundle(),
+      new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+      new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+      new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+      new AppBundle\AppBundle(),
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
-            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
+      // CUSTOM
+      new FOS\RestBundle\FOSRestBundle(),
+      new JMS\SerializerBundle\JMSSerializerBundle(),
 
-        return $bundles;
+    );
+
+    if (in_array($this->getEnvironment(), array('dev', 'test'), TRUE)) {
+      $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+      $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+      $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+      $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
-    }
+    return $bundles;
+  }
+
+  public function registerContainerConfiguration(LoaderInterface $loader) {
+    $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+  }
 }
